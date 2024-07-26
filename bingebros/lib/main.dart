2bd1258f459ac9b9238662947827242bca3f2093
Vishal-Singh2k4 <vishalsingh.palace@gmail.com>
2024-07-26 10:37:34 +0530

    Stay Signed in
    stay signed in is working but need to change ui

--- bingebros/lib/main.dart
+++ bingebros/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'pages/sign_in_page.dart';
 import 'pages/home_page.dart';
 import 'pages/username_setup_page.dart';
@@ -19,11 +20,46 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SignInPage(),
-        '/usernameSetup': (context) => UsernameSetupPage(),
-        '/home': (context) => HomePage(),
+      home: AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+
+    // Stream that listens to authentication changes
+    return StreamBuilder<User?>(
+      stream: _auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasData) {
+          // Check if the user is new or existing
+          return FutureBuilder<User?>(
+            future: _auth.currentUser?.reload().then((_) => _auth.currentUser),
+            builder: (context, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              if (userSnapshot.hasData) {
+                User? user = userSnapshot.data;
+                // If the user is new, navigate to the UsernameSetupPage
+                if (user != null && user.displayName == null) {
+                  return UsernameSetupPage();
+                } else {
+                  return HomePage();
+                }
+              } else {
+                return SignInPage();
+              }
+            },
+          );
+        }
+        return SignInPage();
       },
     );
   }
