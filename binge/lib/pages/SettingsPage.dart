7d6d5830d1442ddab0100eff80bcbd74520a1823
Vishal-Singh2k4 <vishalsingh.palace@gmail.com>
2024-08-09 14:43:25 +0530

    Fixed settings

--- binge/lib/pages/SettingsPage.dart
+++ binge/lib/pages/SettingsPage.dart
@@ -1,32 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:binge/providers/AuthProvider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:binge/routes/routes.dart';
 
 class SettingsPage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthProvider>(context);
+    User? user = _auth.currentUser;
 
     return Scaffold(
-      
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Settings Page'),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Settings Page Content'),
-            SizedBox(height: 20),
-            authProvider.isLoggedIn
-                ? ElevatedButton(
-                    onPressed: () async {
-                      await authProvider.logout();
-                      Navigator.of(context).pushReplacementNamed(Routes.splash);
-                    },
-                    child: Text('Logout'),
-                  )
-                : Text('User not logged in'),
-          ],
-        ),
+        child: user != null
+            ? FutureBuilder<DocumentSnapshot>(
+                future: _firestore.collection('users').doc(user.uid).get(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+                  if (snapshot.hasError) {
+                    return Text('Error fetching user data.');
+                  }
+                  if (!snapshot.hasData || !snapshot.data!.exists) {
+                    return Text('No user data available.');
+                  }
+                  var userData = snapshot.data!.data() as Map<String, dynamic>;
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Username: ${userData['username'] ?? user.email}'),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () async {
+                          try {
+                            await _auth.signOut();
+                            Navigator.pushReplacementNamed(
+                                context, Routes.splash);
+                          } catch (e) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Error signing out. Please try again.'),
+                              ),
+                            );
+                          }
+                        },
+                        child: Text('Logout'),
+                      ),
+                    ],
+                  );
+                },
+              )
+            : Text('No user is signed in'),
       ),
     );
   }
